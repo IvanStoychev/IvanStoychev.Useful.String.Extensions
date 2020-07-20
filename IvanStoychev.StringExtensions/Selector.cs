0d0f69723ce62141840b4e85996295ce15ee0055
Ivan Stoychev <ivanstoychev44@gmail.com>
2020-07-21 01:30:55 +0300

    == Selector.cs == - Removed "tryArgumentException" method. - Redesigned "Substring(this string, string, string, StringInclusionOptions)" to look for an "endString" after the occurrence of "startString" in the original string.

--- IvanStoychev.StringExtensions/Selector.cs
+++ IvanStoychev.StringExtensions/Selector.cs
@@ -24,12 +24,12 @@ namespace IvanStoychev.StringExtensions
         public static string Substring(this string str, string startString, string endString, StringInclusionOptions stringInclusionOptions)
         {
             tryArgumentOutOfRangeException(str, startString, nameof(startString));
-            tryArgumentOutOfRangeException(str, endString, nameof(endString));
 
             int startStringIndex = str.IndexOf(startString);
-            int endStringIndex = str.IndexOf(endString);
+            tryArgumentOutOfRangeException(str.Substring(startStringIndex + startString.Length), endString, nameof(endString));
 
-            tryArgumentException(startStringIndex, nameof(startString), endStringIndex, nameof(endString));
+            int endStringIndex = str.IndexOf(endString, startStringIndex + startString.Length);
+
 
             switch (stringInclusionOptions)
             {
@@ -107,24 +107,6 @@ namespace IvanStoychev.StringExtensions
         {
             if (originalString.IndexOf(substring) == -1)
                 throw new ArgumentOutOfRangeException($"{parameterName}", $"The string given for '{parameterName}' (\"{substring}\") was not found in the original string.");
-        }
-
-        /// <summary>
-        /// Compares "startStringIndex" and "endStringIndex" and, if "endStringIndex" is less than "startStringIndex",
-        /// throws an <see cref="ArgumentException"/> that informs the user the value of .
-        /// </summary>
-        /// <param name="startStringParameterName">The name of the "startString" parameter in the calling method.</param>
-        /// <param name="startStringIndex">The index which marks the start of the substring to be extracted in the calling method.</param>
-        /// <param name="endStringParameterName">The name of the "endString" parameter in the calling method.</param>
-        /// <param name="endStringIndex">The index which marks the end of the substring to be extracted in the calling method.</param>
-        /// <exception cref="ArgumentException">
-        /// Thrown when "endStringIndex" is less than "startStringIndex".
-        /// </exception>
-        static void tryArgumentException(int startStringIndex, string startStringParameterName, int endStringIndex, string endStringParameterName)
-        {
-            if (endStringIndex < startStringIndex)
-                throw new ArgumentException($"The string given for '{endStringParameterName}' occurs at index {endStringIndex} which is before the string given for '{startStringParameterName}', with index {startStringIndex}, in the original string." +
-                    Environment.NewLine + $"The argument provided for '{endStringParameterName}' must occur after the argument provided for '{startStringParameterName}'.");
         }
     }
 }
