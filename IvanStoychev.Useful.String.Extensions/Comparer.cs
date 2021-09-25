ec007f2e43eecd4cb064b70a37803c045412e389
Ivan Stoychev <ivanstoychev44@gmail.com>
2021-09-26 01:43:40 +0300

    Consolidated "Contains" methods: - Consolidated "Contains(StringComparison comparison, IEnumerable<string> keywords)" and "Contains(IEnumerable<string> keywords)" into "Contains(IEnumerable<string> keywords, StringComparison comparison = StringComparison.Ordinal)". - Consolidated "Contains(StringComparison comparison, IEnumerable<char> keywords)" and "Contains(IEnumerable<char> keywords)" into "Contains(IEnumerable<char> keywords, StringComparison comparison = StringComparison.Ordinal)".

--- IvanStoychev.Useful.String.Extensions/Comparer.cs
+++ IvanStoychev.Useful.String.Extensions/Comparer.cs
@@ -11,120 +11,110 @@ namespace IvanStoychev.Useful.String.Extensions
     public static class Comparer
     {
         /// <summary>
-        /// Returns a boolean indicating whether any of the strings in the given IEnumerable occur in this string.
-        /// </summary>
-        /// <param name="str">The string to check.</param>
-        /// <param name="keywords">A collection of strings to seek.</param>
-        /// <returns>
-        /// "true" if any of the "keywords" members occur within this string, or if any of them
-        /// are the empty string (""); otherwise, "false".
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// The "keywords" collection or any of its memebers are null.
-        /// </exception>
-        [Pure]
-        public static bool Contains(this string str, IEnumerable<string> keywords)
-        {
-            return str.Contains(keywords.ToArray());
-        }
-
-        /// <summary>
         /// Returns a boolean indicating whether any of the specified strings occur in this string.
         /// </summary>
         /// <param name="str">The string to check.</param>
-        /// <param name="keywords">The strings to seek.</param>
+        /// <param name="keywords">Strings to seek.</param>
         /// <returns>
-        /// "true" if any of the "keywords" parameters occur within this string, or if any of them
-        /// are the empty string (""); otherwise, "false".
+        /// <see langword="true"/> if any of the <paramref name="keywords"/> occur within this string, or if any of them are the empty string (""); otherwise, <see langword="false"/>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// The "keywords" collection or any of its memebers are null.
+        /// The <paramref name="keywords"/> collection or any of its memebers are null.
         /// </exception>
         [Pure]
         public static bool Contains(this string str, params string[] keywords)
         {
-            foreach (string word in keywords)
-                if (str.Contains(word)) return true;
+            foreach (var word in keywords)
+                if (str.Contains(word))
+                    return true;
 
             return false;
         }
 
         /// <summary>
-        /// Returns a boolean indicating whether any of the characters in the given IEnumerable occur in this string.
+        /// Returns a boolean indicating whether any of the specified strings occur in this string, using the specified comparison rules.
         /// </summary>
         /// <param name="str">The string to check.</param>
-        /// <param name="keychars">A collection of characters to seek.</param>
+        /// <param name="comparison">One of the enumeration values that specifies the rules to use in the comparison.</param>
+        /// <param name="keywords">Strings to seek.</param>
         /// <returns>
-        /// "true" if any of the characters occur within this string; otherwise, "false".
+        /// <see langword="true"/> if any of the <paramref name="keywords"/> occur within this string, or if any of them are the empty string (""); otherwise, <see langword="false"/>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// "keychars" is null.
+        /// The <paramref name="keywords"/> collection or any of its memebers are null.
         /// </exception>
         [Pure]
-        public static bool Contains(this string str, IEnumerable<char> keychars)
+        public static bool Contains(this string str, StringComparison comparison, params string[] keywords)
         {
-            return str.Contains(keychars.ToArray());
+            foreach (var word in keywords)
+                if (str.Contains(word, comparison))
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns a <see langword="bool"/> indicating whether any of the strings in <paramref name="keywords"/> occur in this string, using the specified comparison rules.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <param name="keywords">A collection of strings to seek.</param>
+        /// <param name="comparison">One of the enumeration values that specifies the rules to use in the comparison.</param>
+        /// <returns>
+        /// <see langword="true"/> if any of the <paramref name="keywords"/> members occur within this string, or if any of them
+        /// are the empty string (""); otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="keywords"/> collection or any of its memebers are null.
+        /// </exception>
+        [Pure]
+        public static bool Contains(this string str, IEnumerable<string> keywords, StringComparison comparison = StringComparison.Ordinal)
+        {
+            foreach (var word in keywords)
+                if (str.Contains(word, comparison))
+                    return true;
+
+            return false;
         }
 
         /// <summary>
         /// Returns a boolean indicating whether any of the specified characters occur in this string.
         /// </summary>
         /// <param name="str">The string to check.</param>
-        /// <param name="keychars">The characters to seek.</param>
+        /// <param name="keychars">Characters to seek.</param>
         /// <returns>
-        /// "true" if any of the "keychars" parameters occurs within this string; otherwise, "false".
+        /// <see langword="true"/> if any of the <paramref name="keychars"/> occur within this string; otherwise, <see langword="false"/>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// "keychars" is null.
+        /// <paramref name="keychars"/> is null.
         /// </exception>
         [Pure]
         public static bool Contains(this string str, params char[] keychars)
         {
-            foreach (char ch in keychars)
-                if (str.Contains(ch)) return true;
+            foreach (var ch in keychars)
+                if (str.Contains(ch))
+                    return true;
 
             return false;
         }
 
         /// <summary>
-        /// Returns a boolean indicating whether any of the strings in the given IEnumerable occur in this string,
-        /// using the specified comparison rules.
+        /// Returns a boolean indicating whether any of the specified characters occur in this string, using the specified comparison rules.
         /// </summary>
         /// <param name="str">The string to check.</param>
         /// <param name="comparison">One of the enumeration values that specifies the rules to use in the comparison.</param>
-        /// <param name="keywords">A collection of strings to seek.</param>
+        /// <param name="keychars">Characters to seek.</param>
         /// <returns>
-        /// "true" if any of the "keywords" members occur within this string, or if any of them
-        /// are the empty string (""); otherwise, "false".
+        /// <see langword="true"/> if any of the <paramref name="keychars"/> occur within this string; otherwise, <see langword="false"/>.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// "keywords" is null.
+        /// <paramref name="keychars"/> is null.
         /// </exception>
         [Pure]
-        public static bool Contains(this string str, StringComparison comparison, IEnumerable<string> keywords)
+        public static bool Contains(this string str, StringComparison comparison, params char[] keychars)
         {
-            return str.Contains(comparison, keywords.ToArray());
-        }
-
-        /// <summary>
-        /// Returns a boolean indicating whether any of the specified strings occur in this string,
-        /// using the specified comparison rules.
-        /// </summary>
-        /// <param name="str">The string to check.</param>
-        /// <param name="comparison">One of the enumeration values that specifies the rules to use in the comparison.</param>
-        /// <param name="keywords">The strings to seek.</param>
-        /// <returns>
-        /// "true" if any of the "keywords" parameters occur within this string, or if any of them
-        /// are the empty string (""); otherwise, "false".
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// "keywords" is null.
-        /// </exception>
-        [Pure]
-        public static bool Contains(this string str, StringComparison comparison, params string[] keywords)
-        {
-            foreach (string word in keywords)
-                if (str.Contains(word, comparison)) return true;
+            foreach (var ch in keychars)
+                if (str.Contains(ch, comparison))
+                    return true;
 
             return false;
         }
@@ -133,7 +123,6 @@ namespace IvanStoychev.Useful.String.Extensions
         /// Returns a boolean indicating whether any of the characters in the given IEnumerable occur in this string.
         /// </summary>
         /// <param name="str">The string to check.</param>
-        /// <param name="comparison">One of the enumeration values that specifies the rules to use in the comparison.</param>
         /// <param name="keychars">A collection of characters to seek.</param>
         /// <returns>
         /// "true" if any of the characters occur within this string; otherwise, "false".
@@ -142,29 +131,11 @@ namespace IvanStoychev.Useful.String.Extensions
         /// "keychars" is null.
         /// </exception>
         [Pure]
-        public static bool Contains(this string str, StringComparison comparison, IEnumerable<char> keychars)
+        public static bool Contains(this string str, IEnumerable<char> keychars, StringComparison comparison = StringComparison.Ordinal)
         {
-            return str.Contains(comparison, keychars.ToArray());
-        }
-
-        /// <summary>
-        /// Returns a boolean indicating whether any of the specified characters occur in this string,
-        /// using the specified comparison rules.
-        /// </summary>
-        /// <param name="str">The string to check.</param>
-        /// <param name="comparison">One of the enumeration values that specifies the rules to use in the comparison.</param>
-        /// <param name="keychars">The characters to seek.</param>
-        /// <returns>
-        /// "true" if any of the "keychars" parameters occurs within this string; otherwise, "false".
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// "keychars" is null.
-        /// </exception>
-        [Pure]
-        public static bool Contains(this string str, StringComparison comparison, params char[] keychars)
-        {
-            foreach (char ch in keychars)
-                if (str.Contains(ch, comparison)) return true;
+            foreach (var character in keychars)
+                if (str.Contains(character, comparison))
+                    return true;
 
             return false;
         }
