64ab051cc913f3148058814b23458069fdd2ce25
Ivan Stoychev <ivanstoychev44@gmail.com>
2021-09-25 14:38:31 +0300

    Added tests for the exception throwing of the new "TrimStart", "TrimEnd" and "Trim" methods.

--- IvanStoychev.Useful.String.Extensions.Tests/Remover_Tests_Exceptions.cs
+++ IvanStoychev.Useful.String.Extensions.Tests/Remover_Tests_Exceptions.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace IvanStoychev.Useful.String.Extensions.Tests
 {
-    class Remover_Tests_Exceptions
+    public class Remover_Tests_Exceptions
     {
+        [Fact]
+        public void Trim_Int_ArgumentOutOfRange()
+        {
+            string testString = "123";
+            int amount = 2;
+            int lengthDiff = testString.Length - amount * 2;
+            string expectedMessage = $"The value given for '{nameof(amount)}' (\"{amount}\") is longer than the entire string by {Math.Abs(lengthDiff)}. (Parameter '{nameof(amount)}')";
+
+            void testAction() => testString.TrimStart(amount);
+
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(testAction);
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void TrimStart_Int_ArgumentOutOfRange()
+        {
+            string testString = "123";
+            int amount = 4;
+            int lengthDiff = testString.Length - amount;
+            string expectedMessage = $"The value given for '{nameof(amount)}' (\"{amount}\") is longer than the entire string by {Math.Abs(lengthDiff)}. (Parameter '{nameof(amount)}')";
+
+            void testAction() => testString.TrimStart(amount);
+
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(testAction);
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void TrimEnd_Int_ArgumentOutOfRange()
+        {
+            string testString = "123";
+            int amount = 4;
+            int lengthDiff = testString.Length - amount;
+            string expectedMessage = $"The value given for '{nameof(amount)}' (\"{amount}\") is longer than the entire string by {Math.Abs(lengthDiff)}. (Parameter '{nameof(amount)}')";
+
+            void testAction() => testString.TrimEnd(amount);
+
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(testAction);
+            Assert.Equal(expectedMessage, exception.Message);
+        }
     }
 }
