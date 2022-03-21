fc5bb9c004aac9830500c1ab27dd612a37a97ca9
Ivan Stoychev <ivanstoychev44@gmail.com>
2022-03-22 01:43:31 +0200

    Replacer_Tests_Exceptions.cs - Added.

---
+++ IvanStoychev.Useful.String.Extensions.Tests/Replacer_Tests_Exceptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+
+namespace IvanStoychev.Useful.String.Extensions.Tests;
+
+public class Replacer_Tests_Exceptions
+{
+    [Fact]
+    public void Replace_ArgumentException()
+    {
+        string[] oldStrings = new[] { "asd", "", "asd" };
+        string expectedMessage = "Object reference not set to an instance of an object.";
+
+        void testAction() => "".Replace("", oldStrings);
+
+        var exception = Assert.Throws<NullReferenceException>(testAction);
+        Assert.Equal(expectedMessage, exception.Message);
+    }
+
+    [Fact]
+    public void Replace_ArgumentNullException_Collection()
+    {
+        string[] oldStrings = null;
+        string expectedMessage = "Value cannot be null. (Parameter 'replacement')";
+
+        void testAction() => "".Replace("", oldStrings);
+
+        var exception = Assert.Throws<ArgumentNullException>(testAction);
+        Assert.Equal(expectedMessage, exception.Message);
+    }
+
+    [Fact]
+    public void Replace_ArgumentNullException_Member()
+    {
+        string[] oldStrings = { "asd", null };
+        string expectedMessage = "Value cannot be null. (Parameter 'replacement')";
+
+        void testAction() => "".Replace("", oldStrings);
+
+        var exception = Assert.Throws<ArgumentNullException>(testAction);
+        Assert.Equal(expectedMessage, exception.Message);
+    }
+}
