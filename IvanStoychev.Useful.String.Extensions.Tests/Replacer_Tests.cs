8676aefaaedee4bc8bc36ef5e0cd9b8643f3c3f5
Ivan Stoychev <ivanstoychev44@gmail.com>
2022-04-05 02:37:39 +0300

    Replacer_Tests.cs - Fixed wrong test data.

--- IvanStoychev.Useful.String.Extensions.Tests/Replacer_Tests.cs
+++ IvanStoychev.Useful.String.Extensions.Tests/Replacer_Tests.cs
@@ -28,10 +28,10 @@ public class Replacer_Tests
         {
             return new[]
             {
-                new object[] { "takimata amet erat invidunt diam no vel nonummy", "newWord", new string[] { "AMET", "INVIDUNT", "NO" }, "takimata newWord erat newWord diam newWord vel newWordnummy" },
-                new object[] { "erat consequat euismod justo exerci sed sea tation", "newWord", new List<string>() { "CONSEQUAT", "JUSTO", "SED" }, "erat newWord euismod newWord exerci newWord sea tation" },
-                new object[] { "lorem magna eros ea dolore molestie magna rebum", "newWord", new HashSet<string>() { "MAGNA", "EA", "MOLESTIE" }, "lorem newWord eros newWord dolore newWord newWord rebum" },
-                new object[] { "duo clita dolore clita clita gubergren diam vel", "newWord", new Queue<string>(new string[] { "CLITA", "GUBERGREN" }), "duo newWord dolore newWord newWord newWord diam vel" }
+                new object[] { "takimata amet erat invidunt diam no vel nonummy", "newWord", new string[] { "amet", "invidunt", "no" }, "takimata newWord erat newWord diam newWord vel newWordnummy" },
+                new object[] { "erat consequat euismod justo exerci sed sea tation", "newWord", new List<string>() { "consequat", "justo", "sed" }, "erat newWord euismod newWord exerci newWord sea tation" },
+                new object[] { "lorem magna eros ea dolore molestie magna rebum", "newWord", new HashSet<string>() { "magna", "ea", "molestie" }, "lorem newWord eros newWord dolore newWord newWord rebum" },
+                new object[] { "duo clita dolore clita clita gubergren diam vel", "newWord", new Queue<string>(new string[] { "clita", "gubergren" }), "duo newWord dolore newWord newWord newWord diam vel" }
             };
         }
     }
@@ -42,25 +42,25 @@ public class Replacer_Tests
         {
             return new[]
             {
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCulture, "Control - newWord newWord newWord; Variant - case newWord Archæology" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCulture, "Control - newWord newWord newWord; Variant - case newWord Archæology" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCulture, "Control - newWord newWord newWord; Variant - case newWord Archæology" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }), StringComparison.InvariantCulture, "Control - newWord newWord newWord; Variant - case newWord Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new string[] { "case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCulture, "newWord encyclopædia Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopædia", "ARCHÆOLOGY" }, StringComparison.InvariantCulture, "case newWord Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "Archæology" }, StringComparison.InvariantCulture, "case encyclopædia newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "case", "encyclopædia", "Archæology" }), StringComparison.InvariantCulture, "newWord newWord newWord" },
 
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCultureIgnoreCase, "Control - newWord newWord newWord; Variant - newWord newWord newWord" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCultureIgnoreCase, "Control - newWord newWord newWord; Variant - newWord newWord newWord" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCultureIgnoreCase, "Control - newWord newWord newWord; Variant - newWord newWord newWord" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }), StringComparison.InvariantCultureIgnoreCase, "Control - newWord newWord newWord; Variant - newWord newWord newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new string[] { "case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.InvariantCultureIgnoreCase, "newWord encyclopædia newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopædia", "ARCHÆOLOGY" }, StringComparison.InvariantCultureIgnoreCase, "newWord newWord newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "Archæology" }, StringComparison.InvariantCultureIgnoreCase, "newWord encyclopædia newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "case", "encyclopædia", "Archæology" }), StringComparison.InvariantCultureIgnoreCase, "newWord newWord newWord" },
 
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.Ordinal, "Control - newWord newWord newWord; Variant - case encyclopædia Archæology" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.Ordinal, "Control - newWord newWord newWord; Variant - case encyclopædia Archæology" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.Ordinal, "Control - newWord newWord newWord; Variant - case encyclopædia Archæology" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }), StringComparison.Ordinal, "Control - newWord newWord newWord; Variant - case encyclopædia Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new string[] { "case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.Ordinal, "newWord encyclopædia Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopædia", "ARCHÆOLOGY" }, StringComparison.Ordinal, "case newWord Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "Archæology" }, StringComparison.Ordinal, "case encyclopædia newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "case", "encyclopædia", "Archæology" }), StringComparison.Ordinal, "newWord newWord newWord" },
 
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.OrdinalIgnoreCase, "Control - newWord newWord newWord; Variant - newWord encyclopædia newWord" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.OrdinalIgnoreCase, "Control - newWord newWord newWord; Variant - newWord encyclopædia newWord" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new HashSet<string>() { "Case", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.OrdinalIgnoreCase, "Control - newWord newWord newWord; Variant - newWord encyclopædia newWord" },
-                new object[] { "Control - Case encyclopaedia encyclopedia ARCHÆOLOGY; Variant - case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "Case", "encyclopaedia", "ARCHÆOLOGY" }), StringComparison.OrdinalIgnoreCase, "Control - newWord newWord newWord; Variant - newWord encyclopædia newWord" }
+                new object[] { "case encyclopædia Archæology", "newWord", new string[] { "kase", "encyclopaedia", "ARCHÆOLOGY" }, StringComparison.OrdinalIgnoreCase, "case encyclopædia newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new List<string>() { "Case", "encyclopædia", "ARCHAEOLOGY" }, StringComparison.OrdinalIgnoreCase, "newWord newWord Archæology" },
+                new object[] { "case encyclopædia Archæology", "newWord", new HashSet<string>() { "kase", "encyclopædia", "Archæology" }, StringComparison.OrdinalIgnoreCase, "case newWord newWord" },
+                new object[] { "case encyclopædia Archæology", "newWord", new Queue<string>(new string[] { "case", "encyclopædia", "Archæology" }), StringComparison.OrdinalIgnoreCase, "newWord newWord newWord" }
             };
         }
     }
