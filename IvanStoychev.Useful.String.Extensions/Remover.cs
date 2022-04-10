df247371c7ab4f3bc9968d04f94d72038b7d4623
Ivan Stoychev <ivanstoychev44@gmail.com>
2022-04-10 13:43:18 +0300

    Remover.cs - Removed unnecessary usings.

--- IvanStoychev.Useful.String.Extensions/Remover.cs
+++ IvanStoychev.Useful.String.Extensions/Remover.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Globalization;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace IvanStoychev.Useful.String.Extensions;
