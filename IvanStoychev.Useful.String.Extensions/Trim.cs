b570dc8d101aa6362e9191e464c79afbdad5655c
Ivan Stoychev <ivanstoychev44@gmail.com>
2023-12-07 23:52:52 +0200

    Remove.cs - Renamed from "Remover.cs", added "Remove" method overload.
    Trim.cs - Moved "Trim", "TrimEnd" and "TrimStart" metods from "Remover.cs".

--- IvanStoychev.Useful.String.Extensions/Trim.cs
+++ IvanStoychev.Useful.String.Extensions/Trim.cs
@@ -2,122 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace IvanStoychev.Useful.String.Extensions;
 
-/// <summary>
-/// Contains methods that remove substrings.
-/// </summary>
 public static partial class StringExtensions
 {
-    /// <summary>
-    /// Returns a new string in which all occurrences of the given <paramref name="removeString"/> in the current instance are replaced with <see cref="string.Empty"/>.
-    /// </summary>
-    /// <param name="str">The instance to remove strings from.</param>
-    /// <param name="removeString">Substring to be removed.</param>
-    /// <param name="stringComparison">Comparison rules to use.</param>
-    /// <returns>
-    /// A string that is equivalent to the current string except that all instances of <paramref name="removeString"/> are removed.
-    /// </returns>
-    /// <exception cref="ArgumentException">
-    /// <paramref name="removeString"/> is the empty string ("").
-    /// </exception>
-    /// <exception cref="ArgumentNullException">
-    /// <paramref name="removeString"/> is <see langword="null"/>.
-    /// </exception>
-    [Pure]
-    public static string Remove(this string str, string removeString, StringComparison stringComparison = StringComparison.CurrentCulture)
-    {
-        Validate.EmptyString(removeString);
-        Validate.NullArgument(removeString);
-
-        str = str.Replace(removeString, string.Empty, stringComparison);
-
-        return str;
-    }
-
-    /// <summary>
-    /// Returns a new string in which all occurrences of all members of the given <paramref name="removeStrings"/> in the current instance are removed.
-    /// Occurrences are removed in the same order as the IEnumerable's members, using the provided <paramref name="stringComparison"/> rules.
-    /// </summary>
-    /// <param name="str">The instance to remove strings from.</param>
-    /// <param name="removeStrings">Collection of values to be removed.</param>
-    /// <param name="stringComparison">Comparison rules to use.</param>
-    /// <returns>
-    /// A string that is equivalent to the current string except that all instances of all members of the given collection are removed.
-    /// If none of the members are found in the current instance, the method returns it unchanged.
-    /// </returns>
-    /// <exception cref="ArgumentException">
-    /// Any of the <paramref name="removeStrings"/> members are the empty string ("").
-    /// </exception>
-    /// <exception cref="ArgumentNullException">
-    /// The <paramref name="removeStrings"/> collection or any of its members are <see langword="null"/>.
-    /// </exception>
-    [Pure]
-    public static string Remove(this string str, IEnumerable<string> removeStrings, StringComparison stringComparison = StringComparison.CurrentCulture)
-    {
-        Validate.NullArgument(removeStrings);
-        Validate.IEnumNotEmpty(removeStrings);
-
-        foreach (var item in removeStrings)
-        {
-            Validate.NullMember(item, nameof(removeStrings));
-            Validate.EmptyStringMember(item, nameof(removeStrings));
-            str = str.Replace(item, string.Empty, stringComparison);
-        }
-
-        return str;
-    }
-
-    /// <summary>
-    /// Uses a regular expression to return a new string in which all occurrences of all unicode digits (in any script) in the current instance are removed.
-    /// </summary>
-    /// <param name="originalString">The instance to remove digits from.</param>
-    /// <returns>
-    /// A string that is equivalent to the current string except that all instances of all unicode digits (in any script) are removed.
-    /// If no digits are found in the current instance, the method returns it unchanged.
-    /// </returns>
-    /// <exception cref="RegexMatchTimeoutException">
-    /// The execution time of the replacement operation exceeds the time-out interval specified for the application domain in which the method is called.
-    /// If no time-out is defined in the application domain's properties, or if the time-out value is "Regex.InfiniteMatchTimeout", no exception is thrown.
-    /// </exception>
-    [Pure]
-    public static string RemoveNumbers(this string originalString)
-        => Regex.Replace(originalString, @"[\d-]", string.Empty);
-
-    /// <summary>
-    /// Uses a regular expression to return a new string in which all occurrences of all special characters in the current instance are removed.
-    /// </summary>
-    /// <param name="originalString">The instance to remove special characters from.</param>
-    /// <returns>
-    /// A string that is equivalent to the current string except that all instances of all special characters are removed.
-    /// If no special characters are found in the current instance, the method returns it unchanged.
-    /// </returns>
-    /// <exception cref="RegexMatchTimeoutException">
-    /// The execution time of the replacement operation exceeds the time-out interval specified for the application domain in which the method is called.
-    /// If no time-out is defined in the application domain's properties, or if the time-out value is "Regex.InfiniteMatchTimeout", no exception is thrown.
-    /// </exception>
-    [Pure]
-    public static string RemoveSpecialCharacters(this string originalString)
-        => Regex.Replace(originalString, "[^0-9A-Za-z]+", string.Empty);
-
-    /// <summary>
-    /// Uses a regular expression to return a new string in which all occurrences of all latin letters in the current instance are removed.
-    /// </summary>
-    /// <param name="originalString">The instance to remove latin letters from.</param>
-    /// <returns>
-    /// A string that is equivalent to the current string except that all instances of all latin letters are removed.
-    /// If no latin letters are found in the current instance, the method returns it unchanged.
-    /// </returns>
-    /// <exception cref="RegexMatchTimeoutException">
-    /// The execution time of the replacement operation exceeds the time-out interval specified for the application domain in which the method is called.
-    /// If no time-out is defined in the application domain's properties, or if the time-out value is "Regex.InfiniteMatchTimeout", no exception is thrown.
-    /// </exception>
-    [Pure]
-    public static string RemoveLetters(this string originalString)
-        => Regex.Replace(originalString, "[A-Za-z]", string.Empty);
-
     /// <summary>
     /// Removes all leading and trailing occurrences of a specified string from the current instance, using the provided string comparison option.
     /// </summary>
