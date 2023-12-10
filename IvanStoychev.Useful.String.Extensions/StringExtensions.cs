7673ffbd42d9fe8b6f6df62d34287b56d0212b2d
Ivan Stoychev <ivanstoychev44@gmail.com>
2023-12-10 13:58:21 +0200

    StringExtensions.cs - Added with the explicit purpose to hold only the class summary.

---
+++ IvanStoychev.Useful.String.Extensions/StringExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace IvanStoychev.Useful.String.Extensions;
+
+/// <summary>
+/// Contains additional helpful methods for the <see cref="string"/> class.
+/// </summary>
+public static partial class StringExtensions { }
