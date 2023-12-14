ee07c3e1293d5769772730106b01afa217efe14a
Ivan Stoychev <ivanstoychev44@gmail.com>
2023-12-15 00:51:19 +0200

    Keeper.cs - Added "Validate.OriginalInstanceNotNull" check to all methods.

--- IvanStoychev.Useful.String.Extensions/Keeper.cs
+++ IvanStoychev.Useful.String.Extensions/Keeper.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics.Contracts;
-using System.Linq;
+﻿using System;
+using System.Diagnostics.Contracts;
 using System.Text.RegularExpressions;
 
 namespace IvanStoychev.Useful.String.Extensions;
@@ -16,9 +16,16 @@ public static partial class StringExtensions
     /// Thrown if the execution time of the replacement operation exceeds the time-out interval specified for the application domain in which the method is called.
     /// If no time-out is defined in the application domain's properties, or if the time-out value is "Regex.InfiniteMatchTimeout", no exception is thrown.
     /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The original instance (<paramref name="str"/>) is null.
+    /// </exception>
     [Pure]
-    public static string KeepOnlyNumbers(this string originalString)
-        => Regex.Replace(originalString, @"[^\d]", string.Empty);
+    public static string KeepOnlyNumbers(this string str)
+    {
+        Validate.OriginalInstanceNotNull(str);
+
+        return Regex.Replace(str, @"[^\d]", string.Empty);
+    }
 
     /// <summary>
     /// Returns a new string containing all occurrences of all special characters in the current instance.
@@ -29,9 +36,16 @@ public static partial class StringExtensions
     /// <remarks>
     /// Whitespace is considered a special character.
     /// </remarks>
+    /// <exception cref="ArgumentNullException">
+    /// The original instance (<paramref name="str"/>) is null.
+    /// </exception>
     [Pure]
-    public static string KeepOnlySpecialCharacters(this string originalString)
-        => Regex.Replace(originalString, @"[0-9a-zA-Z]", string.Empty);
+    public static string KeepOnlySpecialCharacters(this string str)
+    {
+        Validate.OriginalInstanceNotNull(str);
+
+        return Regex.Replace(str, @"[0-9a-zA-Z]", string.Empty);
+    }
 
     /// <summary>
     /// Uses a regular expression to return a new string containing all occurrences of all latin letters in the current instance.
@@ -43,7 +57,14 @@ public static partial class StringExtensions
     /// Thrown if the execution time of the replacement operation exceeds the time-out interval specified for the application domain in which the method is called.
     /// If no time-out is defined in the application domain's properties, or if the time-out value is "Regex.InfiniteMatchTimeout", no exception is thrown.
     /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The original instance (<paramref name="str"/>) is null.
+    /// </exception>
     [Pure]
-    public static string KeepOnlyLetters(this string originalString)
-        => Regex.Replace(originalString, @"[^a-zA-Z]", string.Empty);
+    public static string KeepOnlyLetters(this string str)
+    {
+        Validate.OriginalInstanceNotNull(str);
+
+        return Regex.Replace(str, @"[^a-zA-Z]", string.Empty);
+    }
 }
