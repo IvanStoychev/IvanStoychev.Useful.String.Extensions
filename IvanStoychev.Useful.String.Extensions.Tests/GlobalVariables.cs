faaff890cadd3804cf128c69d8f0c46390e6568d
Ivan Stoychev <ivanstoychev44@gmail.com>
2024-01-20 21:36:42 +0200

    GlobalVariables.cs - Removed "CurrentCulture" and "CurrentCultureIgnoreCase" class members.

--- IvanStoychev.Useful.String.Extensions.Tests/GlobalVariables.cs
+++ IvanStoychev.Useful.String.Extensions.Tests/GlobalVariables.cs
@@ -4,8 +4,6 @@ namespace IvanStoychev.Useful.String.Extensions.Tests;
 
 class GlobalVariables
 {
-    internal static StringComparison CurrentCulture = StringComparison.CurrentCulture;
-    internal static StringComparison CurrentCultureIgnoreCase = StringComparison.CurrentCultureIgnoreCase;
     internal static StringComparison InvariantCulture = StringComparison.InvariantCulture;
     internal static StringComparison InvariantCultureIgnoreCase = StringComparison.InvariantCultureIgnoreCase;
     internal static StringComparison Ordinal = StringComparison.Ordinal;
